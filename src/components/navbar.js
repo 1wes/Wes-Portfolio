45e016feee7da4bdba1a9e468da03f848343752b
1wes <okemwawes@gmail.com>
2023-05-26 16:56:55 +0300

    build layout for site navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,48 @@
+import './navbar.css';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDownLong } from '@fortawesome/free-solid-svg-icons';
+import {FaGithub, FaLinkedin, FaTwitter} from 'react-icons/fa'
+
+let Navbar=()=>{
+
+    return(
+
+        <React.Fragment>
+            <nav className='main-navbar'>
+                <div className='logo'>
+                    <p><span>O</span><span>W</span><span>.</span> </p>
+                </div>
+                <div className='nav-menu-content'>
+                    <ul>
+                        <li>Home</li>
+                        <li>Portfolios</li>
+                        <li>Blog
+                            <i>
+                                <FontAwesomeIcon icon={faArrowDownLong} />
+                            </i>
+                        </li>
+                        <li>Contact</li>
+                        <li>
+                            <i>
+                                <FaGithub/>
+                            </i>
+                        </li>
+                        <li>
+                            <i>
+                                <FaTwitter/>
+                            </i>
+                        </li>
+                        <li>
+                            <i>
+                                <FaLinkedin/>
+                            </i>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+        </React.Fragment>
+    )
+}
+export default Navbar;
