b479c7f0e41d4453b0914bfc3943c1a4d4f4af5c
1wes <okemwawes@gmail.com>
2023-05-27 15:11:43 +0300

    import necessary components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Navbar from './components/navbar';
+import IntroSection from './components/intro';
+
+class App extends React.Component{
+
+  render(){
+
+    return(
+      <React.Fragment>
+        <div className='app'>
+          <Navbar/>
+          <IntroSection/>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
