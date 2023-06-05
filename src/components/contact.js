a7025013931f57bce94325b62f341571c10aed3f
1wes <okemwawes@gmail.com>
2023-06-06 01:48:22 +0300

    grab form input details

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './contact.css';
 import { CardSection } from "./services";
 import { CardRow } from "./portfolio";
@@ -7,12 +7,11 @@ import { BsTelephoneFill } from "react-icons/bs";
 import { FaFacebookSquare, FaInstagramSquare, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { GoChevronUp } from "react-icons/go";
 import { SectionHeader } from "./services";
-import { FormLabel, TextField } from "@mui/material";
 
 let ContactDescription=()=>{
 
     const header=`CONTACT ME`;
-    const tagline=`Feel free to reach out to me by filling and submitting the form below and I will get back to you as soon as possible`;
+    const tagline=`Feel free to reach out to me by filling and submitting the form below and I will get back to you as soon as possible.`;
 
     return(
         <>
@@ -55,31 +54,31 @@ let GoUp=({onClick})=>{
     )
 }
 
-let ContactForm=()=>{
+let ContactForm=({onSubmit, onNameChange, onEmailChange, onMessageChange, name, email, message})=>{
 
     return(
         <React.Fragment>
             <div className="contact-form-section">
-                <form className="contact-form">
+                <form className="contact-form" onSubmit={onSubmit}>
                     <label className="form-label">
                         Name
                     </label>
                     <div className="input-field">
-                        <input type="text" placeholder="Enter your name"/>
+                        <input type="text" placeholder="Enter your name" value={name} onChange={onNameChange} required />
                     </div>
                     
                     <label className="form-label">
                         Email
                     </label>
                     <div className="input-field">
-                        <input type="email" placeholder="Enter your email" />
+                        <input type="email" placeholder="Enter your email" value={email} onChange={onEmailChange} required />
                     </div>
 
                     <label className="form-label">
                        Message
                     </label>
                     <div className="input-field">
-                        <textarea autoComplete="off" placeholder="Enter your message"/>
+                        <textarea autoComplete="off" placeholder="Enter your message" value={message} onChange={onMessageChange} required />
                     </div>
                     <div className="cta-btn" id="contact-form-cta">
                         <button type="submit">
@@ -94,16 +93,49 @@ let ContactForm=()=>{
 
 let Contact=()=>{
 
+    const [name, setName]=useState(null);
+    const [email, setEmail]=useState(null);
+    const [message, setMessage]=useState(null);
+
     const scrollToTop=()=>{
         window.scrollTo({top:0, left:0, behavior:"smooth"});
     }
+
+    const handleNameChange=(name=>{
+        setName(name);
+    });
+
+    const handleEmailChange=(email=>{
+        setEmail(email);
+    });
+
+    const handleMessageChange=(message=>{
+        setMessage(message);
+    });
+
+    const submitForm=(e=>{
+
+        e.preventDefault();
+
+        alert(JSON.stringify(name+email+message))
+
+        // let formData={
+        //     sender:name,
+        //     email:email,
+        //     message:message
+        // }
+
+        // alert(JSON.stringify(formData));
+
+    });
+
 
     return(
         <React.Fragment>
             <div className="contact-section" id="contact-me">
                 <div className="contact-content">
                     <ContactDescription/>
-                    <ContactForm/>
+                    <ContactForm onSubmit={submitForm} onNameChange={handleNameChange} onEmailChange={handleEmailChange} onMessageChange={handleMessageChange} />
                     <CardSection id={`contact-card-section`}>
                         <CardRow>
                             <ContactCard contactIcon={<BsTelephoneFill/>} contact={`tel:+254 7 03 239124`} address={`+254 7 03 239124`} />
