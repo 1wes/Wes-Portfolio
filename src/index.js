28140edb89a5cfd70a503c77ceb0c22fc79df8bd
1wes <okemwawes@gmail.com>
2023-06-09 16:02:30 +0300

    add route for specific project using id

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import Project from './components/project';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter hashType='slash'>
     <Routes>
       <Route path='/' element={<App/>}></Route>
+      <Route path='/project/:id' element={<Project/>} ></Route>
     </Routes>
   </HashRouter>
 );
