608d274b2526dd7b8dd61e46121e715e8e544d57
1wes <okemwawes@gmail.com>
2023-06-13 13:17:18 +0300

    add endpoint that gets project by id

--- server/server.js
+++ server/server.js
@@ -4,13 +4,15 @@ const cors=require('cors');
 const { port }=require('./env-config');
 const projects=require('./projects');
 const sendMail=require('./mail');
+const project=require('./project');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({}));
 
 app.use("/", sendMail);
-app.use('/', projects)
+app.use('/', projects);
+app.use('/', project);
 
 app.listen(port, ()=>{
     console.log(`Server started at port ${port}`); 
