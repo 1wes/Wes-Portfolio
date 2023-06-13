115428937bf6aaf8f19cd8f015c7b5142e6c226e
1wes <okemwawes@gmail.com>
2023-06-13 13:18:14 +0300

    export projects array

---
+++ server/projectsList.js
@@ -0,0 +1,31 @@
+
+projects=[{
+    id:1,
+    name:'Accuweights',
+    description:`Accuweights is a renowned technical services provider that caters for various industrial requisites 
+    such as repair and calibration of measuring equipment. I revamped their website to give it a more modern look and feel.`,
+    livelink:`https://www.accuweights.com/`,
+    codebase:`https://github.com/1wes/1wes.github.io`,
+    techstack:['React', 'NodeJs', 'Express', 'Git'],
+    snippet:'accuweights.png'
+},{
+    id:2,
+    name:'Replica KABU Student Portal',
+    description:`I fully replicated the Kabarak University student portal. The front-end is entirely similar to the actual portal,
+     while I used the student flow within the system to try and build the backend.`,
+    livelink:``,
+    codebase:``,
+    techstack:``,
+    snippet:'kabu.png'
+},{
+    id:3,
+    name:'Tic-Tac-Toe',
+    description:`I built this popular game to learn and master several advanced React principles. 
+    The game has a minimalist look, with plans underway to enable online multi-players.`,
+    livelink:``,
+    codebase:``,
+    techstack:`` ,
+    snippet:'tictactoe.png'
+}];
+
+module.exports=projects;
