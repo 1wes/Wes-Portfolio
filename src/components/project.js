c7720b9947ad09b329aa25dd8333760d153b5994
1wes <okemwawes@gmail.com>
2023-06-19 14:48:46 +0300

    display project case study

--- src/components/project.js
+++ src/components/project.js
@@ -4,64 +4,50 @@ import Navbar from './navbar';
 import Footer from "./footer";
 import { useLocation } from "react-router-dom";
 import './project.css';
-import { TechStackCard } from "./about";
 import { Link } from "react-router-dom";
 import { FiExternalLink } from "react-icons/fi";
 import { FaGithub } from "react-icons/fa";
-import { Tooltip } from "@mui/material";
-import { CardRow } from "./portfolio";
 
-let ProjectDetails=({children, title, src, overview, codebase, site})=>{
+
+let ProjectDetails=({children, title, src, overview, codebase, site, header})=>{
 
     return(
         <React.Fragment>
-            <div className="project-details-wrapper">
-                <div className="project-intro">
-                    <h1>{title}</h1>
+            <div className="page-header">
+               {header} Case Study
+            </div>
+            <div className="case-study-wrapper">
+                <div className='cs-image'>
+                    <img src={src} alt="case-study"/>
                 </div>
-                <div className="project-snippet">
-                    <img src={src} alt="project snippet" />
-                </div>
-                <div className="centered-container">
-                    
-                    <h1>Summary</h1>
-
-                    <p className="summary-text">
-                        {overview}
-                    </p>
-                    <div className="tech-used">
-                        <h2>Technologies used</h2>
-                        {children}
+                <div className="cs-summary">
+                    <div className="overview">
+                        <h3>{title}</h3>
+                        <p className="case-study">
+                            {overview}
+                        </p>
                     </div>
-
-                    <div className="link-header">
-                        <h2>Demo</h2>
+                    <div className="tech">
+                        <code>{children}</code>
                     </div>
                     <div className="links">
-                        <Tooltip title={`Go to codebase`} placement="bottom" arrow>
-                            <span>
-                                <Link to={codebase}>
-                                    <i>
-                                        <FaGithub/>
-                                    </i>
-                                </Link>
-                            </span>
-                        </Tooltip>
-                        <Tooltip title={`Go to site`} placement="bottom" arrow>
-                            <span>
-                                <Link to={site}>
-                                    <i>
-                                    <   FiExternalLink/>
-                                    </i>
-                                </Link>
-                        </span>
-                        </Tooltip>
+                        <Link to={codebase} target="blank">
+                            <i>
+                                <FaGithub/>
+                            </i>
+                        </Link>
+                        <Link to={site} target="blank">
+                            <i>
+                                <FiExternalLink/>
+                            </i>
+                        </Link>
                     </div>
                 </div>
             </div>
         </React.Fragment>
     )
 }
+
 
 let Project=()=>{
 
@@ -93,14 +79,15 @@ let Project=()=>{
             <div className="project-section">
                 <div className="project-content">
                     {
-                        project!==null?<ProjectDetails title={project.name} overview={project.description} codebase={project.codebase} src={require(`../${project.snippet}`)}>
+
+                        project!==null?<ProjectDetails header={project.name} title={project.name} overview={project.description} codebase={project.codebase} site={project.livelink} src={require(`../${project.casestudyImg}`)}>
                         {                            
-                            project.techstack && project.techstack.map(language=>{
+                            project.techstack.map(language=>{
 
                                 return (
-                                    <CardRow key={project.id}>
-                                        <TechStackCard techname={language}/>
-                                    </CardRow>
+                                    <span>
+                                        {language}
+                                    </span>
                                 )
                             })
                         }
