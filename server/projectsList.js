04e8408445c5ef931c82daec246fbab7fc4f3c7d
1wes <okemwawes@gmail.com>
2023-06-19 14:48:02 +0300

    add more properties to the project object

--- server/projectsList.js
+++ server/projectsList.js
@@ -6,26 +6,29 @@ projects=[{
     such as repair and calibration of measuring equipment. I revamped their website to give it a more modern look and feel.`,
     livelink:`https://www.accuweights.com/`,
     codebase:`https://github.com/1wes/1wes.github.io`,
-    techstack:['React', 'NodeJs', 'Express', 'Git'],
-    snippet:'accuweights.png'
+    techstack:['Javascript', 'jQuery', 'HTML', 'CSS', 'Semantic UI', 'GitHub'],
+    snippet:'accuweights.png',
+    casestudyImg:'accuweigh.png'
 },{
     id:2,
     name:'Replica KABU Student Portal',
     description:`I fully replicated the Kabarak University student portal. The front-end is entirely similar to the actual portal,
      while I used the student flow within the system to try and build the backend.`,
     livelink:``,
-    codebase:``,
-    techstack:``,
-    snippet:'kabu.png'
+    codebase:`https://github.com/1wes/KABU-shadow-student-portal`,
+    techstack:['React.js', 'Node.js', 'Express.js', 'HTML', 'CSS', 'Git'],
+    snippet:'kabu.png',
+    casestudyImg:'kaba.png'
 },{
     id:3,
     name:'Tic-Tac-Toe',
     description:`I built this popular game to learn and master several advanced React principles. 
     The game has a minimalist look, with plans underway to enable online multi-players.`,
-    livelink:``,
-    codebase:``,
-    techstack:`` ,
-    snippet:'tictactoe.png'
+    livelink:`https://1wes.github.io/`,
+    codebase:`https://github.com/1wes/1wes.github.io`,
+    techstack:['React.js', 'Javascript', 'HTML', 'CSS', 'Git'] ,
+    snippet:'tictactoe.png',
+    casestudyImg:'tictac.png'
 }];
 
 module.exports=projects;
