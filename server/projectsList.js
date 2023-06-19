a09b84022211961083f6fc4f885020baad149204
1wes <okemwawes@gmail.com>
2023-06-19 15:32:07 +0300

    create new propoerty for the case study

--- server/projectsList.js
+++ server/projectsList.js
@@ -4,6 +4,11 @@ projects=[{
     name:'Accuweights',
     description:`Accuweights is a renowned technical services provider that caters for various industrial requisites 
     such as repair and calibration of measuring equipment. I revamped their website to give it a more modern look and feel.`,
+    caseStudy:`The Accurate Weigtech company website was developed over a decade ago. Since then, newer tools, design principles, and UX guidelines
+    had hit the market. Consequently, it was necessary for the website to have a newer and more modern look and feel to help the company maintain an online presence
+    and retain its clients. I helped redesign the website, adding a few more features it lacked before, such as the map indicating the exact location of the company
+    premises. The new user interface was inspired by design ideas from other websites in the same industry as my client's. Feel free to check out the project via 
+    the links provided below `,
     livelink:`https://www.accuweights.com/`,
     codebase:`https://github.com/1wes/1wes.github.io`,
     techstack:['Javascript', 'jQuery', 'HTML', 'CSS', 'Semantic UI', 'GitHub'],
@@ -14,6 +19,12 @@ projects=[{
     name:'Replica KABU Student Portal',
     description:`I fully replicated the Kabarak University student portal. The front-end is entirely similar to the actual portal,
      while I used the student flow within the system to try and build the backend.`,
+    caseStudy:`For a long time, I had been fascinated by Javascript and its application in real-world scenarios. Having been introduced to the language as soon as 
+    I began learning web development, I was a bit down-fallen that I could not use it across the board to build a full-stack application. However, as I delved deeper
+    into developing web applications, I came across Node.js and the MERN tech stack. In a bid to master this stack and finally use Javascript for both 
+    frontend and backend, I decided to replicate my Alma Mater, Kabarak University,'s student portal. This project provided me with a real test on how to implement
+    my Javascript, problem-solving, and critical-thinking skills. The development of this portal is still underway and open for contributions from other Javascript
+    developers`,
     livelink:``,
     codebase:`https://github.com/1wes/KABU-shadow-student-portal`,
     techstack:['React.js', 'Node.js', 'Express.js', 'HTML', 'CSS', 'Git'],
@@ -24,6 +35,9 @@ projects=[{
     name:'Tic-Tac-Toe',
     description:`I built this popular game to learn and master several advanced React principles. 
     The game has a minimalist look, with plans underway to enable online multi-players.`,
+    caseStudy:`Tic Tac Toe is  popular two-person game played across the world. Having already used React for over an year, I decided to re-learn the 
+    core library basics using this game. Currently, the game can only be played by two people in the same location. I plan to introduce a multiplayer, 
+    online feature soon`,
     livelink:`https://1wes.github.io/`,
     codebase:`https://github.com/1wes/1wes.github.io`,
     techstack:['React.js', 'Javascript', 'HTML', 'CSS', 'Git'] ,
