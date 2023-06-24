b33a844806548aa10d011f1da20dab04deefa9f2
1wes <okemwawes@gmail.com>
2023-06-24 17:15:21 +0300

    add socials to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,8 @@ import React from "react";
 import './footer.css';
 import { FaRegCopyright } from "react-icons/fa";
 import { LogoImage } from "./navbar";
+import { Link } from "react-router-dom";
+import {FaGithub, FaLinkedin, FaTwitter, FaInstagramSquare} from 'react-icons/fa';
 
 let Footer=()=>{
 
@@ -11,7 +13,39 @@ let Footer=()=>{
             <div className="footer">
                 <div className="footer-content">
                     <div className="footer-image">
-                        <LogoImage/>
+                        <div className="footer-logo">
+                            <LogoImage/>
+                        </div>
+                        <div className="footer-socials">
+                            <li>
+                                <Link to={`https://github.com/1wes`} target='blank'>
+                                    <i>
+                                        <FaGithub/>
+                                    </i>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={`https://www.linkedin.com/in/okemwa-wes/`} target='blank'>
+                                    <i>
+                                        <FaLinkedin/>
+                                    </i>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={`https://twitter.com/okemwa_wes`} target='blank'>
+                                    <i>
+                                        <FaTwitter/>
+                                    </i>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={`https://z-p15.www.instagram.com/w_es.le_y/`}>
+                                    <i>
+                                        <FaInstagramSquare/>
+                                    </i>
+                                </Link>
+                            </li>
+                        </div>
                     </div>
                     <div className="copyright">
                         <i><FaRegCopyright/></i>
